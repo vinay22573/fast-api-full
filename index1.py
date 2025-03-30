47b1eed1fba1938564a52b60ffee60d675274c67
Vinay Kumar Dubey <Vinay22573@iiitd.ac.in>
2025-03-30 18:18:19 +0530

    change of name

--- index1.py
+++ index1.py
@@ -4,23 +4,23 @@ app = FastAPI()
 # if you name it myapp then uvicorn main:myapp --reload
 # if you name it index.py then uvicorn index:app --reload
 # so these two parameters what you name the instace and name of the file are very important
-
+# the name of the function does not matters but route matters
 
 @app.get("/")
 async def root():
     return {"message": "Shree Ganesh!",        
-    "sonOf":
-    {
+        "sonOf":
+        {
         "father":"shri mahadev",
         "mother": "shri parvati"
-    }
-    }
+        }
+        }
 
 @app.get("/about")
 async def root():
     return {
         "bitFumes":"Youtube for Coding",
-}
+            }
 
 # 4 things to be done
 # import fastapi
